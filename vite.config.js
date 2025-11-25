e8c01458a26c160ec2d7eee22324d8ac9a462d7f
GEOJEFF <geojeff@debian.jeff>
2025-11-25 10:25:52 +0300

    Build for github

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: '/product-management-app/'
+  base: '/product-management-app/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true
+  }
 })
